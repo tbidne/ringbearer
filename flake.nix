ef39f173f252e8fec1fb5acee073770cd3f9a9b1
Tommy Bidne <tbidne@protonmail.com>
2021-06-10 14:36:29 +1200

    Add CI checks, remove 'v' from version

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        version = "v1.0.0";
+        version = "1.0.0";
       in
       {
         defaultPackage = with pkgs; stdenv.mkDerivation {
